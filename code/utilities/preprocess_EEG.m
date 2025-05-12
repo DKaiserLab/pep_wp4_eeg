4d86142f728741e8ad95117c5acbcfc1c9422519
Thea Marie Schmitt <140946200+TheaS-01@users.noreply.github.com>
2025-05-12 22:15:35 +0200

    code adjustments

--- code/utilities/preprocess_EEG.m
+++ code/utilities/preprocess_EEG.m
@@ -10,18 +10,22 @@ ss=109; %[1 4:5 7:15 17]
 prestim=0.5;
 baseline=0.5;
 poststim=0.5;
-output_dir='C:\MATLAB\Scene Clips\Scene Clips 2\Data\fieldtrip preprocessing with students\';
+%output_dir='Users/theaschmitt/Documents/Data Science/6. Semester/Thesis/Test/';
+%output_dir = fullfile('Users', 'theaschmitt', 'Documents', 'Data Science', '6. Semester', 'Thesis', 'Test');
+output_dir = fullfile(pwd, '..', '..', 'derivatives');
 
 % enable fieldtrip functions
-
+%restoredefaultpath;
+%addpath ../../../../../../MATLAB/fieldtrip;
 ft_defaults;
 
+%% Start for-loop
 for s=ss% for each subject    
            
     %% Specify input file
     
-    fileName=['C:\MATLAB\Scene Clips\Scene Clips 2\Data\Scene_Clips2_2',num2str(s,'%02.f'),'.eeg'];
-
+    %fileName=['Users/theaschmitt/Documents/Data Science/6. Semester/Thesis/Daten/','PEP_WP4_EEG', num2str(s,'%02.f'),'.eeg'];
+    fileName = fullfile(pwd, '..', '..', 'sourcedata', ['sub-', num2str(s)], 'eeg', ['PEP_WP4_EEG', num2str(s), '.eeg']);
     %% Define Events
 
     cfg=[];
@@ -31,13 +35,13 @@ for s=ss% for each subject
     cfg.trialdef.prestim=prestim;
     cfg.trialdef.poststim=poststim;
     cfg=ft_definetrial(cfg);
-    
-    two_dig_logical=cfg.trl(:,4)<100;
-    two_dig_triggers=cfg.trl(two_dig_logical,4);
-    cfg.trl(two_dig_logical,4)=cellfun(@(c)c(1),""+two_dig_triggers)-'0';
-    three_dig_triggers=cfg.trl(~two_dig_logical,4);
-    cfg.trl(~two_dig_logical,4)=cellfun(@(c)c(2),""+three_dig_triggers)-'0';
-    
+%     
+%     two_dig_logical=cfg.trl(:,4)<100;
+%     two_dig_triggers=cfg.trl(two_dig_logical,4);
+%     cfg.trl(two_dig_logical,4)=cellfun(@(c)c(1),""+two_dig_triggers)-'0';
+%     three_dig_triggers=cfg.trl(~two_dig_logical,4);
+%     cfg.trl(~two_dig_logical,4)=cellfun(@(c)c(2),""+three_dig_triggers)-'0';
+%     
     %% Load Data and Preprocess Data
     
     cfg.hpfilter='no';
@@ -60,6 +64,11 @@ for s=ss% for each subject
     cfg=[];
     cfg.viewmode='vertical';
     cfg=ft_databrowser(cfg,data);
+
+    if isfield(cfg, 'trl')
+        cfg = rmfield(cfg, 'trl');
+    end
+
     data=ft_rejectartifact(cfg,data);
 
     %% Remove bad trials / channels
@@ -101,9 +110,9 @@ for s=ss% for each subject
     
     % transform to "timelocked" data and save the output
     cfg=[];
-    cfg.outputfile=[output_dir 'scene_clips_2_stud_timelock',num2str(s)];
+    cfg.outputfile=fullfile(output_dir, ['sub-', num2str(s)], 'eeg', ['PEP_WP4_EEG', num2str(s), '_timelock']);
     cfg.keeptrials='yes';
-    save([output_dir 'scene_clips_2_stud_non_timelocked',num2str(s)],'data');
+    save(fullfile(output_dir, ['sub-', num2str(s)], 'eeg', ['PEP_WP4_EEG', num2str(s), '_timelock']),'data');
     data=ft_timelockanalysis(cfg,data);
     
     % if you want: display the data
