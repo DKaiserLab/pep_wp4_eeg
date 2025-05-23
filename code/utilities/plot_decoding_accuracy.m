80cd154d1cfbc9c1174ec4d7290f488eaed7b8f3
Thea Marie Schmitt <thea.marie.schmitt@datascience.uni-giessen.de>
2025-05-23 12:46:20 +0200

    Update plot_decoding_accuracy.m

--- code/utilities/plot_decoding_accuracy.m
+++ code/utilities/plot_decoding_accuracy.m
@@ -3,16 +3,18 @@ clear
 clc
 close all
 
-slist=[6];
+%% define parameter
+slist=[6]; % smoothing window
 
+%% plot decoding accuracy
 for s=slist
 
-    %% load data
+    % load data
     filepath = fullfile(pwd, '..', '..', 'derivatives', ['PEP_WP4_EEG_decoding_accuracy', num2str(s), '_TP_RA.mat']);
     load(filepath);
 
 
-    %% plot decoding results
+    % plot decoding results
 
     dec_acc_mean=squeeze(mean(res.dec_acc,1));
 
@@ -24,14 +26,12 @@ for s=slist
     yline(max(dec_acc_mean), 'k--', ['max ', num2str(max(dec_acc_mean))])
     yline(chance,'k--');
     xline(0,'k');
-    title(['eeg decoding']);
+    title(['decoding accuracy']);
 
-    %%
+    % save plot
+    output_dir=fullfile('..','..', 'Plots');
+    filename = fullfile(output_dir, ['decoding_accuracy', num2str(s), '.jpg']);
 
-    output_dir=fullfile('..','..','..', 'Plots');
-    filename = fullfile(output_dir, ['decoding_accuracy_without110', num2str(s), '.jpg']);
-
-    % Speichern
-    saveas(gcf, filename);  % oder: print(gcf, filename, '-djpeg', '-r300');
+    saveas(gcf, filename); 
 
 end
