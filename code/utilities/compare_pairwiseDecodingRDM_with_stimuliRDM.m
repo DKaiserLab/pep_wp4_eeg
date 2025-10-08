c97562311816cf84b19df02e5b70f67687559027
Thea Marie Schmitt <thea.marie.schmitt@datascience.uni-giessen.de>
2025-10-08 09:58:21 +0200

    Update compare_pairwiseDecodingRDM_with_stimuliRDM.m

--- code/utilities/compare_pairwiseDecodingRDM_with_stimuliRDM.m
+++ code/utilities/compare_pairwiseDecodingRDM_with_stimuliRDM.m
@@ -1,10 +1,9 @@
 function stimuli = compare_pairwiseDecodingRDM_with_stimuliRDM(cfg,d)
 % evaluate input
-if ~isfield(cfg, 'RDM_to_partial_out'); cfg.RDM_to_partial_out = {'typical_late', 'control_late'}; end
 if ~isfield(cfg, 'correlation_type'); cfg.correlation_type = 'pearson';end
 if ~isfield(cfg, 'add_legend'); cfg.add_legend = true;end
 if ~isfield(cfg, 'show_single_cate'); cfg.show_single_cate = false;end
-if ~isfield(cfg, 'partial_cor'); cfg.partial_cor = true;end
+if ~isfield(cfg, 'partial_cor'); cfg.partial_cor = false;end
 if ~isfield(cfg, 'save_name'); cfg.save_name = 'compare_tp_RDMs_to_stimuli_RDMs';end
 if ~isfield(cfg, 'xaxis_labels'); cfg.xaxis_labels = true;end
 if ~isfield(cfg, 'dnns'); cfg.dnns = {cfg.dnn};end
@@ -37,7 +36,7 @@ for iSub = 1:length(cfg.subNums)
     for c = cfg.categories
         category = char(c);
 
-        stimuli.(subID).(category) = nan(numel(cfg.RDM_to_partial_out), numel(d.(cfg.ISC_type).included_time));
+        stimuli.(subID).(category) = nan(numel(cfg.predictor_RDMs), numel(d.(cfg.ISC_type).included_time));
 
         % get canditate/predictor RDMs
         RDMs = struct;
@@ -57,16 +56,16 @@ for iSub = 1:length(cfg.subNums)
                 RDMs(1).RDM = d.pairRep.(['sub', num2str(cfg.subNums(iSub))]).rdm(51:100, 51:100, iTp);
             end
 
-            if cfg.partial_cor
-                [~, rMat, ~, cfg] = partial_cor_RDM(cfg, RDMs);
-            else
-                [~, rMat, ~] = cor_RDM(RDMs,cfg);
-            end
+            %             if cfg.partial_cor
+            %                 [~, rMat, ~, cfg] = partial_cor_RDM(cfg, RDMs);
+            %             else
+            [~, rMat, ~] = cor_RDM(RDMs,cfg);
+            %             end
 
             % save results
             stimuli.(subID).(category)(:, iTp) = rMat(2:end, 1);
         end % tp
- 
+
         for j = 1:length(cfg.predictor_RDMs)
             pred = char(cfg.predictor_RDMs{j});
             stimuli.all.(pred).(category)(iSub,:) = stimuli.(subID).(category)(j,:);
