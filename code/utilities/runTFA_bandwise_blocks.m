10ea0c5e85cccdc3c9781177bc7cd2b615865827
michaengeser <125463198+michaengeser@users.noreply.github.com>
2026-03-18 16:28:37 +0100

    Update runTFA_bandwise_blocks.m

--- code/utilities/runTFA_bandwise_blocks.m
+++ code/utilities/runTFA_bandwise_blocks.m
@@ -1,48 +1,73 @@
 function runTFA_bandwise_blocks(exp_cfg)
 
 if ~isfield(exp_cfg, 'normalize'); exp_cfg.normalize = false; end
+if ~isfield(exp_cfg, 'cutResponseWindow'); exp_cfg.cutResponseWindow = true; end
 
 for s = exp_cfg.subNums
 
     %% -------------------------
-    % Paths
+    % Paths and filenames
     % -------------------------
 
     prepro_path = fullfile(exp_cfg.outputPath, ['sub-', num2str(s)], 'eeg');
     tfa_path = prepro_path;
+    beh_path = fullfile(exp_cfg.sourcedataPath, ['sub-', num2str(s)], 'beh');
+
     prepro_filename = ['PEP_WP4_EEG', num2str(s), '_blocks.mat'];
-    tfa_filename = ['PEP_WP4_EEG', num2str(s), '_tfa_block_',...
-        num2str(1), '.mat'];
+    beh_filename = ['sub-', num2str(s), '_task-main_events.tsv'];
+    tfa_filename_all = ['PEP_WP4_EEG', num2str(s), '_tfa_blocks_all.mat'];
+
 
     %% -------------------------
     % Skip if exists
     % -------------------------
 
-    if exist(fullfile(tfa_path, tfa_filename), 'file')
-        fprintf(['File already exists: ' tfa_filename '\n']);
+    if exist(fullfile(tfa_path, tfa_filename_all), 'file')
+        fprintf(['File already exists: ' tfa_filename_all '\n']);
         continue;
     else
-        fprintf(['Start tfa for: ' tfa_filename '\n']);
+        fprintf(['Start tfa for: ' tfa_filename_all '\n']);
     end
 
     %% -------------------------
     % Load data
     % -------------------------
 
+    % Load data
     load(fullfile(prepro_path, prepro_filename));
 
-    for iBlk = 1:height(timelock.trial)
+    if exp_cfg.cutResponseWindow
+        % Load log file
+        behav = readtable(fullfile(beh_path, beh_filename), 'FileType', 'text');
 
-        % Now 5 bands: delta, theta, alpha, beta, gamma
+        % Get sampling info
+        fileNameIn = fullfile(exp_cfg.sourcedataPath, ['sub-', num2str(s)], ...
+            'eeg', ['PEP_WP4_EEG', num2str(s), '.eeg']);
+        hdr = ft_read_header(fileNameIn);
+        Fs  = hdr.Fs;
+        events = ft_read_event(fileNameIn);
+        events = events(strcmp({events.type}, 'Stimulus'));
+    end
+
+    %% -------------------------
+    % Loop blocks
+    % -------------------------
+
+    % Sore tfa of all blocks in one cell
+    tf_all = cell(1, height(timelock.trial));
+
+    for blk = 1:height(timelock.trial)
+
+        % 5 bands: delta, theta, alpha, beta, gamma
         tf = cell(1, 5);
 
         % Adjust file name
         tfa_filename = ['PEP_WP4_EEG', num2str(s), '_tfa_block_',...
-            num2str(iBlk), '.mat'];
+            num2str(blk), '.mat'];
 
         % Filter for current block
         cfg = [];
-        cfg.trials = iBlk;
+        cfg.trials = blk;
         data_block = ft_selectdata(cfg, timelock);
 
         % Remove NaN timepoints
@@ -186,10 +211,6 @@ for s = exp_cfg.subNums
             tf{5} = newTf;
         end
 
-        %% -------------------------
-        % Cleanup
-        % -------------------------
-
         clear newTf
 
         %% -------------------------
@@ -201,13 +222,65 @@ for s = exp_cfg.subNums
         tf = ft_appendfreq(ft_cfg, tf{:});
 
         %% -------------------------
-        % Save
+        % Save blocks tfa
         % -------------------------
 
         save(fullfile(tfa_path, tfa_filename), 'tf', '-v7.3');
 
-        clear tf
+        tf_all{blk} = tf;
 
+
+        clear tf
+    end
+    %% -------------------------
+    % Cut Response windows
+    % -------------------------
+
+    if exp_cfg.cutResponseWindow
+
+        for blk = 1:height(timelock.trial)
+
+            % Get current tf
+            blkTf = tf_all{blk};
+
+            % Get events of current block
+            blkTrials = behav(behav.block == blk, :);
+            blkEvents = events(blkTrials.trial + (blk - 1)*height(blkTrials));
+
+            % Mark time windows that are good
+            taskFreeTPs = true(1, length(blkTf.time));
+
+            targetCount = 0;
+            for i = 1:height(blkTrials)-1
+
+                % Only trials WITH response
+                if ~isnan(blkTrials.responseTime(i))
+                    targetCount = targetCount +1;
+
+                    % Define start/end in seconds
+                    t_start = blkEvents(i).sample - blkEvents(1).sample + 250 + blkTrials.iti(i)*Fs; % + 500 ms
+                    t_end   = blkEvents(i+1).sample - blkEvents(1).sample;
+
+                    % Convert to samples
+                    s_start = t_start / Fs;
+                    s_end   = t_end   / Fs;
+
+                    % Mark time points in this window
+                    windowTPs = blkTf.time < s_end & blkTf.time > s_start;
+                    taskFreeTPs(windowTPs) = false;
+                end
+            end
+            disp(num2str(targetCount))
+
+            % Filter for current block
+            blkTf.time = blkTf.time(logical(taskFreeTPs));
+            blkTf.trial = blk;
+            blkTf.powspctrm = blkTf.powspctrm(:, :, :, logical(taskFreeTPs));
+            tf_all{blk} = blkTf;
+
+        end
+
+        save(fullfile(tfa_path, tfa_filename_all), 'tf_all', '-v7.3');
     end
 
     clear timelock
